30d60065773c0b4947a54016c0dfe8381667007e
wangpuyu <grayson0921@163.com>
2017-12-25 17:51:22 +0900

    version 2
    added some docstring

--- media.py
+++ media.py
@@ -1,15 +1,26 @@
 # -*- coding=UTF-8 -*-
 import webbrowser
 
+
 class Movie():
 	"""
 	This class provides a way to store movie related information
 	"""
-	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
 
-	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        """
+		this is a init function.it defined four features of a movie.
+		:param movie_title:
+		:param movie_storyline:
+		:param poster_image:
+		:param trailer_youtube:
+		:return Nothing
+		"""
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        """this function can open the movie's trailer"""
+        webbrowser.open(self.trailer_youtube_url)
